6f11f3886f0e33450d83ce0340947ad9efbb460f
Aleksandr <for.melnik.dev@gmail.com>
2022-08-06 15:52:25 +0300

    add api

--- src/api.js
+++ src/api.js
@@ -1,24 +1,28 @@
 const API_KEY =
     "444e219b9ab566cb08816218971dd62b24e68c4f824f8ffd78ddcbdddbced0b2";
 
-const tickersHandlers = new Map();
+const tickersHandlers = new Map(); // {}
 
+//TODO: refactor to use URLSearchParams
 const loadTickers = () => {
     if (tickersHandlers.size === 0) {
         return;
     }
 
     fetch(
-        `https://min-api.cryptocompare.com/data/pricemulti?fsym=${[...tickersHandlers.keys()].join(",")}&tsyms=USD&api_key=${API_KEY}`
+        `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${[
+            ...tickersHandlers.keys()
+        ].join(",")}&tsyms=USD&api_key=${API_KEY}`
     )
         .then(r => r.json())
         .then(rawData => {
             const updatedPrices = Object.fromEntries(
                 Object.entries(rawData).map(([key, value]) => [key, value.USD])
             );
+
             Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-                const handler = tickersHandlers.get(currency) ?? [];
-                handler.forEach(fn => fn(newPrice));
+                const handlers = tickersHandlers.get(currency) ?? [];
+                handlers.forEach(fn => fn(newPrice));
             });
         });
 };
@@ -32,5 +36,7 @@ export const unsubscribeFromTicker = ticker => {
     tickersHandlers.delete(ticker);
 };
 
-setInterval(loadTickers, 5000)
-window.tickersHandlers = tickersHandlers
+setInterval(loadTickers, 5000);
+
+window.tickers = tickersHandlers;
+
